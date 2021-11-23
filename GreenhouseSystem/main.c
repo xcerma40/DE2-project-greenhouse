68f887ad4b971378bf709424f74e30d60e128809
cermi8 <v.cermak98@seznam.cz>
2021-11-23 18:49:01 +0100

    Initial commit

---
+++ GreenhouseSystem/main.c
@@ -0,0 +1,18 @@
+/*
+ * GreenhouseSystem.c
+ *
+ * Created: 23.11.2021 18:44:48
+ * Author : cerma
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
